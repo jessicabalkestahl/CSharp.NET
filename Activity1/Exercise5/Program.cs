bad5fe3c938a51162fb180f6d3ef813b56956593
Jessica Balkestahl <jessicabalkestahl@gmail.com>
2023-11-13 21:46:04 +0000

    [Activity 1]
    (ADDED) Exercises 5 and 6
    (CHANGED) Exercises 1 and 2 now use int's instead of longs

---
+++ Activity1/Exercise5/Program.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Exercise5
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var numbers = new List<double>();
+            
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                    continue;
+                };
+                
+                Console.WriteLine($"[Mean] {CalculateMean(numbers)}");
+                Console.WriteLine($"[Sum]  {CalculateSum(numbers)}");
+                Console.WriteLine($"[Max]  {CalculateHighestNumber(numbers)}");
+                Console.WriteLine($"[Min]  {CalculateLowestNumber(numbers)}");
+                return;
+            }
+                
+        }
+        
+        private static double CalculateMean(List<double> numbers)
+        {
+            double sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum / numbers.Count;
+        }
+        
+        private static double CalculateSum(List<double> numbers)
+        {
+            double sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        
+        private static double CalculateHighestNumber(List<double> numbers)
+        {
+            double highest = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num <= highest) continue;
+
+                highest = num;
+            }
+            return highest;
+        }
+        
+        private static double CalculateLowestNumber(List<double> numbers)
+        {
+            double lowest = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num >= lowest) continue;
+
+                lowest = num;
+            }
+            return lowest;
+        }
+    }
+}
---
+++ Activity1/Exercise6/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System.Text.Json.Nodes;
+
+namespace Exercise6
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.Write("[Calculator] ");
+            string mathExpression = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine($"{mathExpression} = {PerformMathOperation(mathExpression)}");
+            
+        }
+
+        private static double PerformMathOperation(string expression)
+        {
+            int operatorIndex = GetOperatorIndex(expression);
+            double number1 = double.Parse(SplitString(expression, 0, operatorIndex));
+            double number2 = double.Parse(SplitString(expression, operatorIndex + 1, expression.Length));
+
+            return expression[operatorIndex] switch
+            {
+                '+' => Sum(number1, number2),
+                '-' => Sub(number1, number2),
+                '*' => Mult(number1, number2),
+                '/' => Div(number1, number2),
+                _ => -1
+            };
+        }
+
+        private static string SplitString(string expression, int startingIndex, int endingIndex)
+        {
+            string result = "";
+            
+            for (int i = startingIndex; i < endingIndex; i++)
+            {
+                result += expression[i];
+            }
+
+            return result;
+        }
+
+        private static int GetOperatorIndex(string expression)
+        {
+            if (expression.Contains('+')) return expression.IndexOf('+');
+            if (expression.Contains('-')) return expression.IndexOf('-');
+            if (expression.Contains('*')) return expression.IndexOf('*');
+            if (expression.Contains('/')) return expression.IndexOf('/');
+
+            return -1;
+        }
+
+        private static double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+        
+        private static double Sub(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+        
+        private static double Mult(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+        
+        private static double Div(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
+
+
