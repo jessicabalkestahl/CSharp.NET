b5fdc3976dc91ad4fa6ef9a0d2f88bbed34e26f2
Jessica Balkestahl <jessicabalkestahl@gmail.com>
2023-11-20 21:19:58 +0000

    [Activity 2]
    (ADDED) Exercises 1 - 6 from Activity 2

---
+++ Activity2/Exercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Activity2;
+
+internal class Exercise1
+{
+    public static void Main()
+    {
+        double[] numbers = new double[5];
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Enter number {i + 1}: ");
+            numbers[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("");
+        double sum = 0; 
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += numbers[i];
+            }
+            else
+            {
+                Console.WriteLine($"[ Odd index {i} ] {numbers[i]}");
+            }
+        }
+        
+        Console.WriteLine($"[ Sum of even numbers ] {sum}");
+        
+        
+
+        
+    }
+}
---
+++ Activity2/Exercise5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise5;
+
+internal class Exercise5
+{
+    public static void Main()
+    {
+        List<string?> countries = new List<string?>();
+        
+        while (true)
+        {
+            Console.Write("Country: ");
+            string? input = Console.ReadLine();
+
+            if (input == "STOP")
+            {
+                PrintLongCountries(countries, 8);
+                return;
+            }
+            countries.Add(input);
+        }
+    }
+
+    private static void PrintLongCountries(List<string?> countries, int limit)
+    {
+        Console.WriteLine();
+        foreach (string? country in countries)
+        {
+            if (country != null && country.Length < limit) continue;
+            
+            string c = $"Country: {country}".PadRight(25);
+            Console.WriteLine($"{c} | Length: {country.Length}");
+        }        
+    }
+    
+}
