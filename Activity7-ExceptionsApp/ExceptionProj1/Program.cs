9a88057968b8bf7f207ceb029fbe3e6ad891b7ab
Jessica Balkestahl <jessicabalkestahl@gmail.com>
2023-12-05 09:12:01 +0000

    [Activity 7]
    (ADDED) Exercises 1 and 2

---
+++ Activity7-ExceptionsApp/ExceptionProj1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionProj1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            SumNumbers();
+        }
+
+        private static void SumNumbers()
+        {
+            try
+            {
+                int firstNumber, secondNumber, sum = 0;
+                Console.Write("First number: ");
+                firstNumber = int.Parse(Console.ReadLine()!);
+                Console.Write("Second number: ");
+                secondNumber = int.Parse(Console.ReadLine()!);
+
+                sum = firstNumber + secondNumber;
+                Console.WriteLine($"Sum: {sum}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An error has occured! Use integers as in your inputs.");
+                Console.WriteLine($"Error message: {e.Message}");
+                Console.WriteLine("Enter any key to continue ...");
+                Console.ReadKey();
+                Console.Clear();
+                SumNumbers();
+            }
+        }
+    }
+}
+
+
---
+++ Activity7-ExceptionsApp/ThrowException/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ThrowException
+{
+    internal class Program 
+    {
+        internal static void Main()
+        {
+            Employee employee = new("Maria", 28);
+            employee.ShowDetails();
+
+            try
+            {
+                employee.ChangeAge(-4);
+                employee.ShowDetails();
+            }
+            catch (InvalidAgeException e)
+            {
+                Console.WriteLine($"The age inserted: {e.GetAge()} is not valid.");
+            }
+        }
+    }
+
+    internal class Employee
+    {
+        private string? Name { get; }
+        private int Age { get; set; }
+
+        public Employee(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int ChangeAge(int newAge)
+        {
+            if (newAge < 0)
+            {
+                throw new InvalidAgeException(newAge);
+            }
+
+            this.Age = newAge;
+            return newAge;
+        }
+
+        public string ShowDetails()
+        {
+            string details = $"Employee: {this.Name} is {this.Age} years old.";
+            Console.WriteLine(details);
+            return details;
+        }
+    }
+
+    [Serializable]
+    internal class InvalidAgeException : ApplicationException
+    {
+        public int Age { get; set; }
+        private string ErrorMessage { get; set; }
+        
+
+        public InvalidAgeException(int age)
+            : base(String.Format("Invalid Age: {0}", age))
+        {
+            this.Age = age;
+            this.ErrorMessage = $"Invalid age: {this.Age}";
+        }
+
+        public int GetAge()
+        {
+            return this.Age;
+        }
+        
+        
+    }
+}
